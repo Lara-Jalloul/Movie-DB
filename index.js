2cf6a362df1fd533063a558ba4369b52c77f6455
Lara-Jalloul <laraadjalloul@gmail.com>
2021-06-14 12:18:37 +0300

    step 3

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const port = 3000
 
 app.get('/', (req, res) => {
     res.send('ok')
-  })
+})
   
-  app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
-  })
+app.listen(port, () => {
+   console.log(`listening at http://localhost:${port}`)
+})
+
+app.get('/test', (req, res) => {
+    res.send('{status:200, message:"ok"}')
+})
+
+app.get('/time', (req, res) => {
+    let date_ob = new Date();
+    let hours = date_ob.getHours();
+    let seconds = date_ob.getSeconds();
+    res.send('{status:200, message:' + hours +":" + seconds + '}')
+})
