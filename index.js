870b2999db472328d9f0c79347dc30036462bcfd
Lara-Jalloul <laraadjalloul@gmail.com>
2021-06-14 13:33:10 +0300

    step 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('ok')
+   res.send('ok')
 })
   
 app.listen(port, () => {
@@ -11,12 +11,29 @@ app.listen(port, () => {
 })
 
 app.get('/test', (req, res) => {
-    res.send('{status:200, message:"ok"}')
+   res.send('{status:200, message:"ok"}')
 })
 
 app.get('/time', (req, res) => {
-    let date_ob = new Date();
-    let hours = date_ob.getHours();
-    let seconds = date_ob.getSeconds();
-    res.send('{status:200, message:' + hours +":" + seconds + '}')
+   let date_ob = new Date();
+   let hours = date_ob.getHours();
+   let seconds = date_ob.getSeconds();
+   res.send('{status:200, message:' + hours +":" + seconds + '}')
+})
+
+app.get('/hello/:userID', function (req, res) {
+    res.send('{status:200, message:Hello,'+req.params.userID+'}')
+})
+
+app.get('/hello', function (req, res) {
+    res.send('{status:200, message:Hello"}')
+})
+
+app.get('/search', function (req, res) {
+const data = req.query.s;
+  if (data != null)
+    res.send('{status: 200,message: "ok",data: data}')
+  else{
+    res.send('{status: 500,error: true,message: "you have to provide a search"}')
+  }
 })
